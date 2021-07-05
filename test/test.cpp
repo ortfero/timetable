aa835aecbe5d3eba5534fbdad425c09ff57d9cff
ortfero <ortfero@gmail.com>
2021-07-05 15:30:42 +0300

    Test snippet

--- test/test.cpp
+++ test/test.cpp
@@ -3,14 +3,23 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 
+#include <chrono>
+#include <cstdio>
+#include <thread>
 #include <timetable/timetable.hpp>
 
 
 
 TEST_CASE("snippet") {
-  timetable::scheduler<> scheduler;
-  scheduler.every(std::chrono::seconds{3},
-                  [](){});
-  scheduler.run();
-  scheduler.stop();
+    using namespace std::chrono;
+    timetable::scheduler scheduler{std::chrono::milliseconds{500}};
+    
+    scheduler.every(seconds{10},
+                    [](){ std::puts("every second"); });
+    scheduler.daily_at_utc(hours{12} + minutes{12},
+                    [](){ std::puts("at 12:12:00 UTC"); });
+
+    scheduler.run();
+    std::this_thread::sleep_for(std::chrono::minutes{3});
+    scheduler.stop();
 }
